56498e9a26e9c8d7c0356962722906e2561c75aa
kevorklepedjian1 <99024590+kevorklepedjian1@users.noreply.github.com>
2024-01-21 12:19:30 +0200

    Add files via upload

---
+++ src/data/songsData.js
@@ -0,0 +1,12 @@
+// src/data.js
+import song1 from '../assets/a.mp3';
+import song2 from '../assets/b.mp3';
+
+
+const songs = [
+  { id: 1, artist: 'Artist 1', trackId: '123', songName: 'Song 1', audioFile: song2 },
+  { id: 2, artist: 'Artist 2', trackId: '456', songName: 'Song 2', audioFile: song1 },
+  // Add more songs as needed
+];
+
+export default songs;
